5b3b9e29972bd39000af438e3724cf116927bae4
Rahul721999 <rrmondal12@gmail.com>
2024-04-27 21:26:27 +0530

    deployed first solana program

---
+++ hello_solana/src/lib.rs
@@ -0,0 +1,21 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg,
+};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8]
+) -> ProgramResult{
+    // this is how we log msg to the blockchain
+    msg!("Hellow solana!");
+    
+    // gracefully exit the program
+    Ok(())
+}
