7a6201448bcefbab0b277710189cb90e3a9886c4
Rahul721999 <rrmondal12@gmail.com>
2024-05-30 23:46:11 +0530

    show balance of given wallet

---
+++ sol-balance/index.ts
@@ -0,0 +1,31 @@
+import { config } from "dotenv";
+config();
+import { Connection, PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+export const solBalance = async (address: String) => {
+  try {
+    const publickey = new PublicKey(address);
+    const conn = new Connection(process.env.SOLANA_RPC_URL!, "confirmed");
+
+    const accountInfo = await conn.getAccountInfo(publickey);
+    // If the account info exists, calculate the balance in SOL and return it
+    if (accountInfo !== null) {
+      return accountInfo.lamports / LAMPORTS_PER_SOL;
+    } else {
+      throw new Error("Account not found");
+    }
+  } catch (error) {
+    // Handle errors
+    console.error("Error fetching Solana account balance:", error);
+    throw error;
+  }
+};
+
+(async () => {
+  try {
+    const balance = await solBalance(process.env.WALLET_SECRET_KEY!);
+    console.log(balance);
+  } catch (error) {
+    console.log('Error: ',error);
+  }
+})();
