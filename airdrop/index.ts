7d2afb0ed6e8735f8db9583eb99d7f363bdcfc58
Rahul721999 <rrmondal12@gmail.com>
2024-06-01 23:17:14 +0530

    Refactor: modified airdrop fn

--- airdrop/index.ts
+++ airdrop/index.ts
@@ -4,14 +4,13 @@ import {PublicKey, Connection, LAMPORTS_PER_SOL} from "@solana/web3.js";
 
 /// project to airdrop some solana on given wallet address
 
-export const airdrop_sol =  async (address: string, amount: number) => {
-    const publickey = new PublicKey(address);
+export const airdrop_sol =  async (publicKey: PublicKey, amount: number) => {
     const connection = new Connection(process.env.SOLANA_RPC_URL!, "confirmed");
 
     // airdrop signature    
-    let airdropSignature = await connection.requestAirdrop(publickey, amount * LAMPORTS_PER_SOL);
+    let airdropSignature = await connection.requestAirdrop(publicKey, amount * LAMPORTS_PER_SOL);
     await connection.confirmTransaction(airdropSignature);
 }
 
 // wallet address, where airdroping solana.
-airdrop_sol(process.env.WALLET_SECRET_KEY!, 1);
+airdrop_sol(new PublicKey(process.env.WALLET_SECRET_KEY!), 1);
