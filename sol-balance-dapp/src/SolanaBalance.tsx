ac4e1b0b16d71dbc0ff5a809e5d77dbe37a92c29
Rahul721999 <rrmondal12@gmail.com>
2024-06-12 20:41:48 +0530

    feature: added airdrop sol button to UI

--- sol-balance-dapp/src/SolanaBalance.tsx
+++ sol-balance-dapp/src/SolanaBalance.tsx
@@ -6,13 +6,20 @@ export const Balance: React.FC = () => {
     const [balance, setBalance] = useState<number | null>(null);
     const [account, setAccount] = useState<string>("");
 
+    // get connection
+    const connection = new Connection(
+        "https://api.devnet.solana.com",
+        "confirmed"
+    );
+    if (connection == null) {
+        console.error("failed to establish connection with BlockChain");
+    }
+
+    // get balance
     const getBalance = async () => {
         try {
-            const connection = new Connection(
-                "https://api.devnet.solana.com",
-                "confirmed"
-            );
             const publicKey = new PublicKey(account);
+
             const balance = await connection.getBalance(publicKey, "confirmed");
             setBalance(balance / 1e9); // Convert lamports to SOL
         } catch (error) {
@@ -20,6 +27,15 @@ export const Balance: React.FC = () => {
         }
     };
 
+    // airdrop solana
+    const airdropSol = async () => {
+        try {
+            const publicKey = new PublicKey(account);
+            await connection.requestAirdrop(publicKey, 2);
+        } catch (err) {
+            console.error("failed to airdrop sol");
+        }
+    };
     return (
         <div>
             <h1>Solana Balance Checker</h1>
@@ -30,6 +46,7 @@ export const Balance: React.FC = () => {
                 placeholder="Enter Solana account address"
             />
             <button onClick={getBalance}>Check Balance</button>
+            <button onClick={airdropSol}>Airdrop 2 Sol</button>
             {balance !== null && (
                 <div>
                     <h2>Balance: {balance} SOL</h2>
