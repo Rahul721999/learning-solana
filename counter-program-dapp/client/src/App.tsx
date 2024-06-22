462c02b564685225314fc5a061b52e74c1bc2051
Rahul721999 <rrmondal12@gmail.com>
2024-06-22 21:57:21 +0530

    refactor: counter-program-dapp yarn config

--- counter-program-dapp/client/src/App.tsx
+++ counter-program-dapp/client/src/App.tsx
@@ -1,11 +1,46 @@
-import React from "react";
-import "./App.css";
-import {Calculator} from "./Calculator";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+    ConnectionProvider,
+    WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+    WalletModalProvider,
+    WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import React, { FC, ReactNode, useMemo } from "react";
 
-function App() {
-    return <div className="App">
-        <Calculator/>
-    </div>;
+import "./App.css";
+import { Calculator } from "./Calculator";
+
+export default function App() {
+    return (
+        <Context>
+            <div className="Calculator">
+                <Calculator />
+            </div>
+        </Context>
+    );
 }
 
-export default App;
+const Context: FC<{ children: ReactNode }> = ({ children }) => {
+    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+    const network = WalletAdapterNetwork.Devnet;
+
+    // You can also provide a custom RPC endpoint.
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    const wallets = useMemo(() => [new UnsafeBurnerWalletAdapter()], [network]);
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider
+                wallets={wallets}
+                autoConnect
+            >
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
