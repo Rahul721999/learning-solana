3ea2ae02a665a5fad64b3a82f5baf263646c60fa
Rahul721999 <rrmondal12@gmail.com>
2024-12-12 23:35:36 +0530

    WIP: token-vesting program - added  CreateVestingAccount Struct

--- token-vesting/anchor/programs/tokenvesting/src/lib.rs
+++ token-vesting/anchor/programs/tokenvesting/src/lib.rs
@@ -1,5 +1,10 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
 
+const ANCHOR_DISCRIMINATOR: usize = 8;
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
@@ -15,31 +20,55 @@ pub mod tokenvesting {
         unimplemented!()
     }
 
-
     pub fn claim_tokens(_context: Context<ClaimTokens>) -> Result<()> {
         unimplemented!()
     }
 }
 
 #[derive(Accounts)]
-pub struct CreateVestingAccount{
+#[instruction(company_name: String)]
+pub struct CreateVestingAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
 
+    #[account(mint::token_program = token_program)]
+    pub token_mint: InterfaceAccount<'info, Mint>,
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = ANCHOR_DISCRIMINATOR + VestingAccount::INIT_SPACE,
+        seeds = [company_name.as_ref()],
+        bump
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+
+    #[account(
+        init,
+        payer = signer,
+        token::mint = mint,
+        token::authority = treasury_token_account,
+        seeds =  [b"vesting_treasury", company_name.as_bytes()],
+        bump
+    )]
+    pub treasury_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
-#[derive(Accounts)]
-pub struct CreateEmployeeAccount{
-
-}
-#[derive(Accounts)]
-pub struct ClaimTokens{
-
-}
-
 
 #[account]
-pub struct EmployeeAccount{
+#[derive(InitSpace, Debug)]
+pub struct VestingAccount {}
 
-}
+
+
+#[derive(Accounts)]
+pub struct CreateEmployeeAccount {}
+#[derive(Accounts)]
+pub struct ClaimTokens {}
+
 #[account]
-pub struct VestingAccount{
-    
-}
+#[derive(InitSpace, Debug)]
+pub struct EmployeeAccount {}
