407697eb2ca941d015f1f0133b544257db84d9d3
Rahul721999 <rrmondal12@gmail.com>
2025-01-31 09:31:00 +0530

    WIP: Lending protocol - fix: `#[constrant] fn or associated item not found in usize`

--- Lending-DApp/lending/programs/lending/src/constants.rs
+++ Lending-DApp/lending/programs/lending/src/constants.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 
-#[constant]
 pub const ANCHOR_DISCRIMINATOR: usize = 8;
+#[constant]
 pub const SOL_USD_FEED_ID: &str =
     "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d";
 pub const USDC_USD_FEED_ID: &str =
